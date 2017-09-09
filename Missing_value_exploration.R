3fab7a707dc7c501477e7355b1661c8ff5839d14
tap222 <mtapas.mohanty95@gmail.com>
2017-09-09 21:15:50 +0000

    Update Missing_value_exploration.R

--- Missing_value_exploration.R
+++ Missing_value_exploration.R
@@ -1,8 +1,8 @@
-setwd('/home/tapas')
+rm(list = ls()); gc()
+setwd('/home/tapas/')
 data = read.csv('cs-training.csv', header =T)
-data1 = read.csv('outlier.csv', header =T)
-data$X<-NULL
-##Library######
+data = data[ ,-1]
+
 library(VIM)
 library(mice)
 library(corrplot)
@@ -15,22 +15,27 @@ library(RColorBrewer)
 library(psych)
 library(ggpubr)
 library(reshape2)
-library(Amelia)
+library(Amelia) ##missmap
 
 ###Data Summary####
-describe(data
 describe(data)
 str(data)
-
- ##columnwise percentage of missing values
-pMiss <- function(x){sum(is.na(x))/length(x)*100}  # to find percentage of the missing column vise,
-apply(tr,2,pMiss)
-apply(te,2,pMiss)
+##Rowwise miss value
+sort(sapply(data, function(x) { sum(is.na(x)) }), decreasing=TRUE)
 ####Plot missing value####
-missmap(data)
+missmap(data, y.labels = NULL, y.at = NULL, col = c("#CC3399", "#6699FF"), main = "Missing values vs observed")
 aggr(data,prop = F, numbers = T)
 md.pattern(data)
 matrixplot(data, interactive = F)
+ggplot(data, aes(data$SeriousDlqin2yrs,fill=!is.na(data$MonthlyIncome))) + geom_bar(position="dodge") + labs(title="Monthly Income",fill="Has Monthly Income")
+ggplot(data, aes(data$SeriousDlqin2yrs,fill=!is.na(data$NumberOfDependents))) + geom_bar(position="dodge") + labs(title="Number_of_dependent",fill="Has Dependent Income")
+
+
+library(reshape2)
+library(ggplot2)
+
+library(reshape2)
+library(ggplot2)
 
 ggplot_missing <- function(x){
   
@@ -49,6 +54,8 @@ ggplot_missing <- function(x){
          y = "Rows / observations")
 }
 
+#Let’s test it out
+
 ggplot_missing(data)
 
 
@@ -57,16 +64,12 @@ corrplot(cor(data[complete.cases(data),]), method="circle", type = "lower", orde
          col=colorRampPalette(brewer.pal(11,"Spectral"))(8))
 cor(data[complete.cases(data),])
 
-corrplot(cor(data1[complete.cases(data1),]), method="circle", type = "lower", order="hclust",
-         col=colorRampPalette(brewer.pal(11,"Spectral"))(8))
-cor(data1[complete.cases(data1),])
 
 ####Heat map for correlation####
 qplot(x=Var1, y=Var2, data=melt(cor(data[complete.cases(data),], use="p")), fill=value, geom="tile") +
   scale_fill_gradient2(limits=c(-1, 1))
-qplot(x=Var1, y=Var2, data=melt(cor(data1[complete.cases(data1),], use="p")), fill=value, geom="tile") +
-  scale_fill_gradient2(limits=c(-1, 1))
-####Principal Component Analysis#####
+
+####PCA#####
 fa.parallel(data, #The data in question.
             fa = "pc", #Display the eigenvalues for PCA.
             n.iter = 100) #Number of simulated analyses to perform.
@@ -78,23 +81,4 @@ pc_bodies
 
 heatmap <- qplot(x=Var1, y=Var2, data=melt(cor(data)), geom="tile",
                  fill=value)
-
-fa.parallel(data1, #The data in question.
-            fa = "pc", #Display the eigenvalues for PCA.
-            n.iter = 100) #Number of simulated analyses to perform.
-abline(h = 1) #Adding a horizontal line at 1.
-pc_bodies = principal(data1, #The data in question.
-                      nfactors = 4, #The number of PCs to extract.
-                      rotate = "none")
-pc_bodies
-pca <- prcomp(data, scale=T,na.action = na.omit())
-melted <- cbind(variable.group, melt(pca$rotation[,1:5]))
-
-barplot <- ggplot(data=data) +
-  geom_bar(aes(x=Var1, y=value, fill=variable.group), stat="identity") +
-  facet_wrap(~Var2)
-
-####Important variables####
-fit=randomForest(factor(SeriousDlqin2yrs)~., data=data[complete.cases(data),])
-varImp(fit)
-varImpPlot(fit,type=2)
+heatmap
