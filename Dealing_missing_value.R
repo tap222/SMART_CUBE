dd553c1ded3a89b4cbe4c1cc430dc2fa401cca47
tap222 <mtapas.mohanty95@gmail.com>
2017-09-11 18:51:10 +0000

    Update Dealing_missing_value.R

--- Dealing_missing_value.R
+++ Dealing_missing_value.R
@@ -10,7 +10,7 @@ test.data = read.csv('cs-test.csv')
 set.seed(123)
 dd <- knnImputation(train.data[,-1],k=5)
 train.data[,2:11] <- dd
-
+write.csv(test,'cs-train-missing.csv')
 set.seed(123)
 dt <- knnImputation(test.data[,-1],k=5)
 test.data[,2:11] <- dt
@@ -59,8 +59,35 @@ hist(train$NumberOfDependents, freq=F, main='NumberOfDependents: Original Data',
      col='darkgreen', ylim=c(0,0.04))
 hist(imp.train_raw$NumberOfDependents, freq=F, main='NumberOfDependents: MICE Output', 
      col='lightgreen', ylim=c(0,0.04))
-write.csv(test,'cs-test-missing.csv')
+write.csv(train,'cs-test-missing.csv')
 
+imp.test_raw <- mice(train, m=5, method='cart', printFlag=FALSE)
+xyplot(imp.test_raw, train$MonthlyIncome ~ train$NumberOfDependents)
+densityplot(imp.test_raw, ~test$MonthlyIncome)
+densityplot(imp.test_raw, ~test$NumberOfDependents)
+imp.train_raw_mean <- mice(test_raw, 
+                           m=5, 
+                           defaultMethod=c('mean', 'cart', 'cart', 'cart'),
+                           printFlag=FALSE)
+xyplot(imp.test_raw_mean, test$MonthlyIncome~ test$NumberOfDependents)
+
+train_complete <- complete(imp.test_raw)
+
+#Confirm no NAs
+sum(sapply(test_complete, function(x) { sum(is.na(x)) }))
+# Plot MonthlyIncome distributions
+par(mfrow=c(1,2))
+hist(test$MonthlyIncome, freq=F, main='MonthlyIncome: Original Data', 
+     col='darkgreen', ylim=c(0,0.04))
+hist(imp.test_raw$MonthlyIncome, freq=F, main='MonthlyIncome: MICE Output', 
+     col='lightgreen', ylim=c(0,0.04))
+# Plot Number of Dependents distributions
+par(mfrow=c(1,2))
+hist(test$NumberOfDependents, freq=F, main='NumberOfDependents: Original Data', 
+     col='darkgreen', ylim=c(0,0.04))
+hist(imp.test_raw$NumberOfDependents, freq=F, main='NumberOfDependents: MICE Output', 
+     col='lightgreen', ylim=c(0,0.04))
+write.csv(test,'cs-test-missing.csv')
 ######################### Rpart ########################################################
 library(plyr);      # load plyr prior to dplyr to avoid warnings
 library(caret);     # A nice machine learning wrapper library 
@@ -79,5 +106,20 @@ model_f   <- train( NumberOfDependents ~ RevolvingUtilizationOfUnsecuredLines + 
                     data = train %>% filter(!is.na(NumberOfDependents)),
                     trControl = trControl, method="rpart", na.action = na.pass, tuneLength = 5)
 train$NumberOfDependents[dependentmiss] = predict(model_f, train[dependentmiss,])
+rpart.plot(model_f$finalModel)
+write.csv(train,'cs-train-missing.csv')
+
+trControl <- trainControl(method="repeatedcv", number=7, repeats=5) 
+Incomemiss <- which(is.na(MonthlyIncome))
+model_f   <- train(MonthlyIncome ~ RevolvingUtilizationOfUnsecuredLines + age + NumberOfOpenCreditLinesAndLoans,
+                    data = test %>% filter(!is.na(MonthlyIncome)),
+                    trControl = trControl, method="rpart", na.action = na.pass, tuneLength = 5)
+train$MonthlyIncome[Incomemiss] = predict(model_f, train[Incomemiss,])
+rpart.plot(model_f$finalModel)
+dependentmiss <- which(is.na(NumberOfDependents))
+model_f   <- train( NumberOfDependents ~ RevolvingUtilizationOfUnsecuredLines + age + NumberOfOpenCreditLinesAndLoans,,
+                    data = test %>% filter(!is.na(NumberOfDependents)),
+                    trControl = trControl, method="rpart", na.action = na.pass, tuneLength = 5)
+test$NumberOfDependents[dependentmiss] = predict(model_f, test[dependentmiss,])
 rpart.plot(model_f$finalModel)
 write.csv(test,'cs-test-missing.csv')
