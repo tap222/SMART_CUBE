aa081afbeae9933fdf495b17e8f6e492cbcdc4f1
tap222 <mtapas.mohanty95@gmail.com>
2017-09-11 11:54:00 +0000

    Create Feature_003.R

---
+++ Feature_003.R
@@ -0,0 +1,55 @@
+setwd('/home/tapas')
+data = read.csv('cs-training-feature-02.csv',header =T)
+test = read.csv('cs-test-feature-02.csv',header =T)
+r=c(0.105,0.089,0.119)
+
+sum = sum(r)
+
+w1 = r[1]/sum
+w2 = r[2]/sum
+w3 = r[3]/sum
+
+default_time = w1 * data$NumberOfTime3059DaysPastDueNotWorse +w2 * data$NumberOfTime6089DaysPastDueNotWorse + w3 * data$NumberOfTimes90DaysLate
+DebtAmt = data$DebtRatio * as.numeric(data$MonthlyIncome)
+
+new = data %>% filter(MonthlyIncome != 0)%>%group_by(NumberOfDependents) %>% summarise(avg=median(MonthlyIncome,na.rm=T))
+
+DepdRisk = 1:150000
+for(i in 1:150000){
+  if(is.na(data$MonthlyIncome[i])){
+    DepdRisk[i] = NA
+  }else{
+    if(data$MonthlyIncome[i] == 0){
+      DepdRisk[i] = data$NumberOfDependents[i]/(new[new$NumberOfDependents==data$NumberOfDependents[i],]$avg)
+    }else{
+      DepdRisk[i] = data$NumberOfDependents[i]/as.numeric(data$MonthlyIncome[i])
+    }
+  }
+}
+
+data = as.data.frame(cbind(data,AgeRisk,default_time,DebtAmt,DepdRisk))
+data = data[,-c(4,5,6,7,9,11,12)]
+write.csv(data,'cs-training-feature-03.csv')
+
+########################Test#######################################
+
+default_time = w1 * test$NumberOfTime3059DaysPastDueNotWorse +w2 * test$NumberOfTime6089DaysPastDueNotWorse + w3 * test$NumberOfTimes90DaysLate
+DebtAmt = test$DebtRatio * as.numeric(test$MonthlyIncome)
+new = test %>% filter(MonthlyIncome != 0)%>%group_by(NumberOfDependents) %>% summarise(avg=median(MonthlyIncome,na.rm=T))
+
+DepdRisk = 1:101503
+for(i in 1:101503){
+  if(is.na(test$MonthlyIncome[i])){
+    DepdRisk[i] = NA
+  }else{
+    if(test$MonthlyIncome[i] == 0){
+      DepdRisk[i] = test$NumberOfDependents[i]/(new[new$NumberOfDependents==test$NumberOfDependents[i],]$avg)
+    }else{
+      DepdRisk[i] = test$NumberOfDependents[i]/as.numeric(test$MonthlyIncome[i])
+    }
+  }
+}
+test = as.data.frame(cbind(test,AgeRisk,default_time,DebtAmt,DepdRisk))
+test = test[,-c(4,5,7,9,11,12)]
+
+write.csv(test,'cs-test-feature-03.csv')
